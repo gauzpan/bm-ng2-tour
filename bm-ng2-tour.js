19f7bc862c8422917fb22ab906ae71d132370554
Gaurav Pandvia <gaurav.pandvia@minjar.com>
2017-05-18 15:27:05 +0530

    Version change

---
+++ bm-ng2-tour.js
@@ -0,0 +1,5 @@
+"use strict";
+var bm_tour_module_1 = require('./src/lib/bm-tour.module');
+exports.BmTourModule = bm_tour_module_1.BmTourModule;
+var bm_tour_service_1 = require('./src/lib/services/bm-tour.service');
+exports.BmTourService = bm_tour_service_1.BmTourService;
